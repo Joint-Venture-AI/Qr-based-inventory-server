1a1318673d8d48bf621ba5722f31aaed6a7a41ad
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-08-18 16:39:07 +0600

    added personalization model

---
+++ src/app/modules/personalization/personalization.model.ts
@@ -0,0 +1,33 @@
+import { model, Schema } from 'mongoose';
+import { IPersonalization } from './personalization.interface';
+
+const personalizationSchema = new Schema<IPersonalization>(
+  {
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: false,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    name: {
+      type: String,
+      required: false,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Personalization = model<IPersonalization>(
+  'Personalization',
+  personalizationSchema
+);
